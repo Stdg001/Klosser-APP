7057bc0ed62df7c42faec6633585afb6d951328a
paper-plane12pixel <ivan.kishk@gmail.com>
2025-08-23 15:37:12 +0200

    vgvhh

---
+++ htdocs/src/components/LevelButton.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Lesson from "./Lesson"
+import { FaTimes } from "react-icons/fa"
+
+function LevelButton({ missionJSON, isActive }) {
+  const [click, setClick] = useState(false)
+
+  return (
+    <div>
+      <button
+        onClick={() => setClick(true)}
+        className="bg-amber-600 p-4 border-2 rounded-full w-20 h-20 font-bold text-3xl hover:cursor-pointer">
+        {missionJSON.level}
+      </button>
+
+      {click && (
+        <div className="fixed inset-0 w-screen h-screen bg-white z-50 p-4">
+            <FaTimes  className="text-3xl" onClick={() => setClick(false)}/>
+            <div className="pt-10"><Lesson leccionJSON={missionJSON}/></div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default LevelButton
--- htdocs/src/pages/Home.jsx
+++ htdocs/src/pages/Home.jsx
@@ -1,12 +1,24 @@
 import Mission from '../components/Lesson';
 import misionLlevarMesa from '../assets/missions/bring-table.json';
 import { FaStar } from 'react-icons/fa';
+import MissionBtn from "../components/LevelButton"
+
 
 const Home = () => {
+
+  function handleClick(click){
+
+  }
   return (
     <div className='p-10 theme-bg h-screen'>
       {/* <Mission leccionJSON={misionLlevarMesa} /> */}
+      <MissionBtn 
+      missionJSON={misionLlevarMesa}/>
+      
+      
+      
     </div>
+
   )
 }
 
