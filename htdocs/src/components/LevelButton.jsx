7bf3d0aac80b8bc812e102e6b50b351f87e67d20
Stdg001 <holasoybarbiequetal1090@gmail.com>
2025-08-23 15:54:58 +0200

    Update 3

--- htdocs/src/components/LevelButton.jsx
+++ htdocs/src/components/LevelButton.jsx
@@ -9,14 +9,15 @@ function LevelButton({ missionJSON, isActive }) {
     <div>
       <button
         onClick={() => setClick(true)}
-        className="bg-amber-600 p-4 border-2 rounded-full w-20 h-20 font-bold text-3xl hover:cursor-pointer">
+        className="bg-amber-600 hover:bg-amber-100 p-4 border-2 rounded-full w-20 h-20 font-bold text-3xl cursor-pointer">
         {missionJSON.id}
       </button>
 
       {click && (
-        <div className="fixed inset-0 w-screen h-screen bg-white z-50 p-4">
-            <FaTimes  className="text-3xl" onClick={() => setClick(false)}/>
-            <div className="pt-10"><Lesson leccionJSON={missionJSON}/></div>
+        <div className="fixed inset-0 w-screen h-screen theme-bg z-50 p-4 ">
+            <FaTimes  className=" theme-text text-3xl" onClick={() => setClick(false)}/>
+            <div className="pt-10">
+              <Lesson leccionJSON={missionJSON}/></div>
         </div>
       )}
     </div>
